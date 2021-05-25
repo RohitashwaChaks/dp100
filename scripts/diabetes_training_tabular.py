8031014e35a22451b0e3d28f855972cd7b11738e
CaptainNemo <rohitashwachaks@gmail.com>
2021-05-25 17:26:17 +0530

    Added - Working with Data notebook

---
+++ scripts/diabetes_training_tabular.py
@@ -0,0 +1,64 @@
+# Import libraries
+import os
+import argparse
+from azureml.core import Run, Dataset
+import pandas as pd
+import numpy as np
+import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_curve
+
+# Get the script arguments (regularization rate and training dataset ID)
+parser = argparse.ArgumentParser()
+parser.add_argument('--regularization', type=float, dest='reg_rate', default=0.01, help='regularization rate')
+parser.add_argument("--input-data", type=str, dest='training_dataset_id', help='training dataset')
+args = parser.parse_args()
+
+# Set regularization hyperparameter (passed as an argument to the script)
+reg = args.reg_rate
+
+# Get the experiment run context
+run = Run.get_context()
+
+# Get the training dataset
+print("Loading Data...")
+diabetes = run.input_datasets['training_data'].to_pandas_dataframe()
+print('Record Count:', diabetes.shape[0])
+run.log('Record Count', diabetes.shape[0])
+
+# Separate features and labels
+X, y = diabetes[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values, diabetes['Diabetic'].values
+
+# Split data into training set and test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
+
+print('Training Set Size:', X_train.shape[0])
+run.log('Training Set Size', X_train.shape[0])
+
+print('Test Set Size:', X_test.shape[0])
+run.log('Test Set Size', X_test.shape[0])
+
+# Train a logistic regression model
+print('Training a logistic regression model with regularization rate of', reg)
+run.log('Regularization Rate',  np.float(reg))
+model = LogisticRegression(C=1/reg, solver="liblinear").fit(X_train, y_train)
+
+# calculate accuracy
+y_hat = model.predict(X_test)
+acc = np.average(y_hat == y_test)
+print('Accuracy:', acc)
+run.log('Accuracy', np.float(acc))
+
+# calculate AUC
+y_scores = model.predict_proba(X_test)
+auc = roc_auc_score(y_test,y_scores[:,1])
+print('AUC: ' + str(auc))
+run.log('AUC', np.float(auc))
+
+os.makedirs('outputs', exist_ok=True)
+# note file saved in the outputs folder is automatically uploaded into experiment record
+joblib.dump(value=model, filename='outputs/diabetes_model.pkl')
+
+run.complete()
